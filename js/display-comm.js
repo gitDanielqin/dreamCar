328395778dff6b221053a42da2113d88ec3da60a
Danielqin <pheonixqin@qq.com>
2017-03-24 23:04:06 +0800

    2017-3-24

--- js/display-comm.js
+++ js/display-comm.js
@@ -13,7 +13,8 @@ var isLogin=true;
                           {major:"经济",submajor:["国民经济","企业经济"]},
                      ],
                      majorAmount:["1-20","20-100","100-200","200-500"],
-                     props:["重点","本科","专科","高职","民营"]
+                     props:["重点","本科","专科","高职","民营"],
+                     scolars:["博士","硕士","本科","大专"]
                 },
                 inc:{
                      IncScale:["1~20","21~50","51~100","101~500","501~1000","1000人以上"],
@@ -22,7 +23,9 @@ var isLogin=true;
                      pos1:["客服/售前/售后技术支持","客服/售前/售后技术支持","客服/售前/售后技术支持","客服/售前/售后技术支持"],
                      pos2:["平面设计师","动效设计师","游戏设计师","UI设计师","平面设计师","动效设计师","游戏设计师","UI设计师","平面设计师","动效设计师","游戏设计师","UI设计师","平面设计师","动效设计师","游戏设计师","UI设计师"],
                      area1:["IT/通信/电子/互联网","房地产/建筑业","保险","银行","信托/担保/拍卖/典当","其他"],
-                     area2:["IT/通信/电子/互联网","房地产/建筑业","保险","银行","信托/担保/拍卖/典当","其他"]
+                     area2:["IT/通信/电子/互联网","房地产/建筑业","保险","银行","信托/担保/拍卖/典当","其他"],
+                     salary:["1k-3k","3k-5k","5k-8k","8k-12k","12k+"],
+                     welfare:["五险一金","双休","餐补","交通补","带薪年假","节日聚餐"]
                 }
            },
            uniQuery:{
@@ -60,8 +63,24 @@ var isLogin=true;
                publicTime:"",
                trainway:""
            },
+           posQuery:{
+                areas:{
+                     area_1:"",
+                     area_2:""
+                },
+                pos:{
+                     pos_1:"",
+                     pos_2:""
+                },
+                worksexp:"",
+                scolar:"",
+                salary:"",
+                welfare:"",
+                publicTime:""
+           },
            showPosBox:false,
-           showAreaBox:false
+           showAreaBox:false,
+           showWelBox:false
       },
       methods:{
            selPos:function(pos,type){
@@ -69,18 +88,47 @@ var isLogin=true;
                      this.uniQuery.incReq.pos.pos_2 = pos;
                 }else if(type=="inc"){
                       this.incQuery.pos.pos_2 = pos;
+                }else if(type=="pos"){
+                     this.posQuery.pos.pos_2 = pos;
                 }
                 this.showPosBox=false;
            },
            clickPos:function(){
                 this.showPosBox=true;
            },
-           selArea:function(area){
-                this.uniQuery.incReq.areas.area_2 = area;
+           selArea:function(area,type){
+                if(type=="uni"){
+                    this.uniQuery.incReq.areas.area_2 = area;
+                }else if(type=="pos"){
+                     this.posQuery.areas.area_2 = area;
+                }
+
                 this.showAreaBox=false;
            },
            clickArea:function(){
                 this.showAreaBox=true;
+           },
+           clickWel:function(){
+                this.showWelBox=true;
+           },
+           checkEv:function(obj){
+                if($(obj).hasClass("on")){
+                     $(obj).removeClass("on");
+                     $(obj).parent("li").removeClass("on");
+                }else{
+                     $(obj).addClass("on");
+                     $(obj).parent("li").addClass("on");
+                }
+           },
+           submitWel:function(){
+                var selWelfare="";
+                $(".welfare-lis li").each(function(){
+                     if($(this).hasClass("on")){
+                          selWelfare+=$(this).text();
+                     }
+                });
+                this.posQuery.welfare=selWelfare;
+                this.showWelBox=false;
            }
       }
 });
@@ -144,7 +192,7 @@ var appModal = new Vue({
           closeLog:function(){
                this.showLogin=false;
                this.showModal=false;
-          }
+          },
      }
 })
 function _init(){
@@ -169,6 +217,7 @@ function selectInitPos(){
        $(".selectee ul").hide();
       appQuery.showPosBox=false;
       appQuery.showAreaBox=false;
+      appQuery.showWelBox=false;
     })
 }
 
@@ -192,6 +241,9 @@ function selectInitPos(){
             $(this).addClass("on");
             $(this).find("span").show();
         });
+        $(".popBox").click(function(){
+             return false;
+        })
         //申请合作对话框事件绑定
         $(".pageBot").pagination({
             totalData:37,
